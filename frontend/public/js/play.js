88789aafbf6430e4d7248238bb129ccb6dbcc540
eng-george1 <eng.george1@gmail.com>
2022-04-18 21:50:44 -0500

    Audio is working well other audio playlist controlos

--- frontend/public/js/play.js
+++ frontend/public/js/play.js
@@ -1,20 +1,66 @@
- function playSong(songid) {
-  
-    // let result = await fetch(apiurl + "songs/play/"+songid+"/", {
-    //     method: "GET",
-    //     headers: {
-    //       "Content-type": "application/json",
-    //       "Authorization": 'Bearer '+sessionStorage.getItem("token"),
-    //     },
-    //   }).then((res) =>res.json());
-    //   console.log("g");
-    //   let song = result;
-    
-    var audio = document.getElementById('audio');
-  
-    var source = document.getElementById('audioSource');
-    source.src = "http://localhost:3000/songs/play/"+songid;
+let playlist = [];
+let currentsongindex = 0;
+async function playSong(songid) {
+  // let result = await fetch(apiurl + "songs/play/"+songid+"/", {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-type": "application/json",
+  //       "Authorization": 'Bearer '+sessionStorage.getItem("token"),
+  //     },
+  //   }).then((res) =>res.json());
+  //   console.log("g");
+  //   let song = result;
+
+  var audio = document.getElementById("audio");
+
+  var source = document.getElementById("audioSource");
+  source.src = "http://localhost:3000/songs/play/" + songid;
   //console.log(song);
-    audio.load(); //call this to just preload the audio without playing
-    audio.play(); //call this to play the song right away
-  };
+  audio.load(); //call this to just preload the audio without playing
+  audio.play(); //call this to play the song right away
+  let isok = false;
+  let result = await fetch(apiurl + "users/playlist", {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + sessionStorage.getItem("token"),
+    },
+  }).then((res) => {
+    isok = res.ok;
+    return res.json();
+  });
+  let songs = result;
+  playlist = songs;
+  currentsongindex = playlist.findIndex((s) => s.id == songid);
+  console.log(playlist, currentsongindex);
+  document.getElementById("playsongname").innerHTML=playlist[currentsongindex].title;
+}
+
+function playforward() {
+  var audio = document.getElementById("audio");
+  var source = document.getElementById("audioSource");
+  console.log(playlist, currentsongindex);
+  if (currentsongindex == playlist.length - 1) {
+    currentsongindex = 0;
+  } else {
+    currentsongindex = currentsongindex + 1;
+  }
+  source.src = "http://localhost:3000/songs/play/" + playlist[currentsongindex].id;
+  audio.load(); //call this to just preload the audio without playing
+  audio.play(); //call this to play the song right away
+  document.getElementById("playsongname").innerHTML=playlist[currentsongindex].title;
+}
+function playbackward() {
+  var audio = document.getElementById("audio");
+  var source = document.getElementById("audioSource");
+  if (currentsongindex == 0) {
+    currentsongindex = playlist.length - 1;
+  } else {
+    currentsongindex = currentsongindex - 1;
+  }
+
+  source.src = "http://localhost:3000/songs/play/" + playlist[currentsongindex].id;
+  audio.load(); //call this to just preload the audio without playing
+  audio.play(); //call this to play the song right away
+  document.getElementById("playsongname").innerHTML=playlist[currentsongindex].title;
+}
